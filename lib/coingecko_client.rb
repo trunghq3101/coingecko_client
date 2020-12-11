1b623d9bf9f7b1746323417274da1ad0d31dd606
TrungHoang <trunghq3101@gmail.com>
2020-12-11 15:40:42 +0700

    [f] Change to use Faraday

--- lib/coingecko_client.rb
+++ lib/coingecko_client.rb
@@ -1,5 +1,5 @@
 require 'coingecko_client/version'
-require 'excon'
+require 'faraday_middleware'
 require 'json'
 
 module CoingeckoClient
@@ -7,7 +7,6 @@ module CoingeckoClient
 
   # This is a simple client to connect to Coingecko
   class Client
-
     def self.ping
       # Check API server status
       api_call('ping')
@@ -19,12 +18,12 @@ module CoingeckoClient
       coin = coin.downcase
       currency = currency.downcase
       result = api_call("simple/price?ids=#{coin}&vs_currencies=#{currency}")
-      return nil if result.nil? or !result.any?
+      return nil if result.nil? || !result.any?
 
       result[coin][currency]
     end
 
-    def self.token_price(platform_id='ethereum',address,currency)
+    def self.token_price(platform_id = 'ethereum', address, currency)
       api_call("simple/token_price/#{platform_id}?contract_addresses=#{address}&vs_currencies=#{currency}")
     end
 
@@ -57,12 +56,11 @@ module CoingeckoClient
       api_call(request)
     end
 
-    def self.coin_history(coin,currency,days)
+    def self.coin_history(coin, currency, days)
       api_call("coins/#{coin.downcase}/market_chart?vs_currency=#{currency.downcase}&days=#{days}")
     end
 
-    def self.list_exchanges(per_page=0, page=1)
-
+    def self.list_exchanges(per_page = 0, page = 1)
       if per_page == 0
         api_call("exchanges?page=#{page}")
       else
@@ -73,12 +71,10 @@ module CoingeckoClient
     private_class_method def self.api_call(api_request)
       endpoint_head = 'https://api.coingecko.com/api/v3/'
       url = endpoint_head + api_request
-      response = Excon.get(url)
+      response = Faraday.get(url)
       return nil if response.status != 200
 
       JSON.parse(response.body)
     end
   end
-   
-
 end
